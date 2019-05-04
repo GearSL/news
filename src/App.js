3a188e2b39606cda28ad59bfc6fd82fe1ec280cc
Ilyas <ilyas.main@gmail.com>
2019-05-04 18:02:39 +0600

    hello react!

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+    let helloWorld = 'Добро пожаловать в Путь к изучению React!!!';
+    let user = {
+        first_name : 'Ilyas',
+        last_name : 'ismailov',
+        midle_name : 'none',
+        login : 'gear',
+    };
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <h2>{helloWorld}</h2>
+            <p> Welcome {user.login} !</p>
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
     </div>
   );
 }
