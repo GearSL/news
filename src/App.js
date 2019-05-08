09699191b7738859697ab24ee6858062bb25630c
Ilyas <ilyas.main@gmail.com>
2019-05-08 18:01:49 +0600

    add test data and mapping elements

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
 function App() {
-    let helloWorld = 'Добро пожаловать в Путь к изучению React!';
-    let user = {
-        first_name : 'Ilyas',
-        last_name : 'ismailov',
-        midle_name : 'none',
-        login : 'gear',
-    };
     return (
     <div className="App">
         <header className="App-header">
-            <h2>{helloWorld}</h2>
-            <p> Welcome {user.login} !</p>
-            <img src={logo} className="App-logo" alt="logo" />
+            {list.map((item)=>(
+                <div key={item.objectID}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                </div>)
+            )}
+            <img src = {logo} className="App-logo" alt="logo" />
         </header>
     </div>
   );
