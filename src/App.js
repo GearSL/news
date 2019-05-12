c8a8c4f79f20f7373c870fcdca0dc3523ff45c10
Ilyas Ismailov <ilyas.main@mail.ru>
2019-05-12 22:29:31 +0600

    The road to learn page 27 (51)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
 function App() {
-    let helloWorld = 'Добро пожаловать в Путь к изучению React!!!';
-    let user = {
-        first_name : 'Ilyas',
-        last_name : 'ismailov',
-        midle_name : 'none',
-        login : 'gear',
-    };
+    const listElements = list.map((element)=>(
+        <li key={element.objectID}> {element.title} {element.url}</li>)
+    );
     return (
     <div className="App">
         <header className="App-header">
-            <h2>{helloWorld}</h2>
-            <p> Welcome {user.login} !</p>
+            {list.map(function (item) {
+                return (
+                    <div key={item.objectID}>
+                        <span>
+                        <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+                );
+            })}
             <img src={logo} className="App-logo" alt="logo" />
         </header>
     </div>
